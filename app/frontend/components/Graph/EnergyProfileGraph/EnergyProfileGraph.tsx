f67ff5855ee9939c44a0c1fa33a435ec2182e130
besm2 <maria.besteman@witteveenbos.com>
2026-02-10 15:08:31 +0000

    energy profile graph works with dummy data

--- app/frontend/components/Graph/EnergyProfileGraph/EnergyProfileGraph.tsx
+++ app/frontend/components/Graph/EnergyProfileGraph/EnergyProfileGraph.tsx
@@ -1,6 +1,5 @@
 
 import { AreaChart, Area, XAxis, YAxis, ResponsiveContainer,CartesianGrid, Tooltip } from 'recharts';
-//import curve_graph from './curve_graph.json'
 
 export default function EnergyProfileGraph() {
     const data = [
@@ -48,34 +47,31 @@ export default function EnergyProfileGraph() {
   }
 ];
     return (
-        <div className="flex flex-col max-h-[550px] flex-1">
-            <div className="text-center p-8">
+        <div className="flex flex-col h-[550px] flex-1">
+            <div className="text-center p-8 flex-1 flex flex-col">
                 <h3 className="text-lg font-semibold mb-4">Energie Profiel</h3>
-                <p className="text-gray-600">Energie profiel grafiek komt hier..</p>
-        <ResponsiveContainer width="100%" height="100%">
-           <AreaChart
-    
-     
-       width={500}
-                height={300}
-      data={data}
-      margin={{
-        top: 20,
-        right: 0,
-        left: 0,
-        bottom: 0,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis  />
-      <Tooltip />
-      <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
-      <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-      <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
-     
-    </AreaChart>
-    </ResponsiveContainer>
+             
+                <div className="flex-1 min-h-0">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <AreaChart
+                            data={data}
+                            margin={{
+                                top: 20,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                            <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                            <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
         </div>
     );
