a089ac28b62cdb6ee995cb535d8ef75b3247fe51
JANN4 <niels.janssen@witteveenbos.com>
2026-02-11 10:42:57 +0100

    Added resampling and made demand values negative

--- hail/hail/result/graph_curve.py
+++ hail/hail/result/graph_curve.py
@@ -34,51 +34,23 @@ class AbstractResultCurveGraph(AbstractResult):
     def meta(self) -> GraphMeta:
         pass
 
-    @classmethod
-    def _make_metadata(cls, gces: list[GraphCurveElement]) -> GraphCurveMeta:
-        exst_meta: GraphCurveMeta = cls.meta
+    # @classmethod
+    # def _make_metadata(cls, gces: list[GraphCurveElement]) -> GraphCurveMeta:
+    #     exst_meta: GraphCurveMeta = cls.meta
 
-        properties = {}
-        for gce in gces:
-            properties[gce.name] = {"group": gce.group, "demandSupply": gce.demandSupply, "color": gce.color}
+    #     properties = {}
+    #     for gce in gces:
+    #         properties[gce.name] = {"group": gce.group, "demandSupply": gce.demandSupply, "color": gce.color}
 
-        x_tick_labels = hourly_datetime_labels()
+    #     x_tick_labels = hourly_datetime_labels()
         
-        return GraphCurveMeta(
-            title=cls.name if exst_meta.title == "default" else exst_meta.title,
-            unit=cls.unit if exst_meta.unit == "default" else exst_meta.unit,
-            properties=properties,
-            xTickLabels=x_tick_labels,
-            **exst_meta.model_dump(exclude={"title", "unit", "xTickLabels", "properties"}),
-        )
-
-    @classmethod
-    def _make_metadata_for_groups(cls, gces: list[GraphCurveElement]) -> GraphCurveMeta:
-        """Create metadata for grouped data, inferring color and demandSupply from group elements"""
-        exst_meta: GraphCurveMeta = cls.meta
-        groups = cls._group_elements(gces)
-
-        # Create properties for groups, omitting the group attribute itself
-        # Color and demandSupply are inferred from the first element in each group
-        properties = {}
-        for group_name, group_elements in groups.items():
-            # Use the first element to infer color and demandSupply for the group
-            representative_element = group_elements[0]
-            properties[group_name] = {
-                "demandSupply": representative_element.demandSupply, 
-                "color": representative_element.color
-            }
-            # Note: group attribute is omitted as requested
-
-        x_tick_labels = hourly_datetime_labels()
-        
-        return GraphCurveMeta(
-            title=cls.name if exst_meta.title == "default" else exst_meta.title,
-            unit=cls.unit if exst_meta.unit == "default" else exst_meta.unit,
-            properties=properties,
-            xTickLabels=x_tick_labels,
-            **exst_meta.model_dump(exclude={"title", "unit", "xTickLabels", "properties"}),
-        )
+    #     return GraphCurveMeta(
+    #         title=cls.name if exst_meta.title == "default" else exst_meta.title,
+    #         unit=cls.unit if exst_meta.unit == "default" else exst_meta.unit,
+    #         properties=properties,
+    #         xTickLabels=x_tick_labels,
+    #         **exst_meta.model_dump(exclude={"title", "unit", "xTickLabels", "properties"}),
+    #     )
 
     @staticmethod
     def _group_elements(gces: list[GraphCurveElement]) -> dict[str, list[GraphCurveElement]]:
@@ -92,27 +64,60 @@ class AbstractResultCurveGraph(AbstractResult):
         return groups
 
     @staticmethod
-    def transform_data_for_frontend(gces: list[GraphCurveElement]) -> list[dict[str, float | int]]:
-        """Make a curve graph that can be parsed by Recharts with all curve graphs in the context and group them by group attribute"""
+    def transform_data_for_frontend(gces: list[GraphCurveElement], resample_hours: int = 1) -> list[dict[str, float | int]]:
+        """Make a curve graph that can be parsed by Recharts with all curve graphs in the context and group them by group attribute
+        
+        Args:
+            gces: List of GraphCurveElement objects
+            resample_hours: Number of hours to aggregate data into (default: 1 for hourly data)
+                           Can be 3, 6, 12, 24 or any arbitrary number of hours
+        """
         groups = AbstractResultCurveGraph._group_elements(gces)
         
+        # Get original hourly data length
+        original_length = len(gces[0].value)
+        
+        # Calculate resampled length
+        resampled_length = (original_length + resample_hours - 1) // resample_hours  # Ceiling division
+        
         # Initialize the graph data structure
-        graph_data = [{} for _ in range(len(gces[0].value))]
+        graph_data = [{} for _ in range(resampled_length)]
         
         # Aggregate values for each group
         for group_name, group_elements in groups.items():
-            # Sum the values for all elements in this group at each time step
-            for i in range(len(gces[0].value)):
+            # Determine if this group represents demand (should be negative)
+            representative_element = group_elements[0]
+            is_demand = representative_element.demandSupply.lower() in ["demand", "vraag"]
+            
+            # Aggregate values at each resampled time step
+            for resampled_index in range(resampled_length):
                 aggregated_value = 0
-                for gce in group_elements:
-                    aggregated_value += gce.value[i]
-                graph_data[i][group_name] = aggregated_value
+                
+                # Calculate the range of original indices for this resampled period
+                start_hour = resampled_index * resample_hours
+                end_hour = min(start_hour + resample_hours, original_length)
+                
+                # Sum values for all elements in this group over the time period
+                for hour_index in range(start_hour, end_hour):
+                    for gce in group_elements:
+                        value = gce.value[hour_index]
+                        # Make demand values negative
+                        if is_demand:
+                            value = -1*value
+                        aggregated_value += value
+                
+                graph_data[resampled_index][group_name] = aggregated_value
 
         return graph_data
 
     @classmethod
-    def transform_and_create_metadata(cls, gces: list[GraphCurveElement]) -> tuple[list[dict[str, float | int]], GraphCurveMeta]:
-        """Transform data for frontend and create metadata in a single operation to avoid redundancy"""
+    def _make_metadata(cls, gces: list[GraphCurveElement], resample_hours: int = 1) -> GraphCurveMeta:
+        """Transform data for frontend and create metadata in a single operation to avoid redundancy
+        
+        Args:
+            gces: List of GraphCurveElement objects
+            resample_hours: Number of hours to aggregate data into (default: 1 for hourly data)
+        """
         groups = cls._group_elements(gces)
         
         # Transform data for frontend
@@ -134,7 +139,29 @@ class AbstractResultCurveGraph(AbstractResult):
                 "color": representative_element.color
             }
 
-        x_tick_labels = hourly_datetime_labels()
+        # Generate appropriate x-axis labels based on resampling
+
+        # Get original hourly data length and resampled length
+        original_length = len(gces[0].value)
+        resampled_length = (original_length + resample_hours - 1) // resample_hours  # Ceiling division
+
+        if resample_hours == 1:
+            x_tick_labels = hourly_datetime_labels()
+        else:
+            # Create labels for resampled intervals
+            base_labels = hourly_datetime_labels()
+            x_tick_labels = []
+            for i in range(0, len(base_labels), resample_hours):
+                if resample_hours == 24:
+                    # For daily aggregation, show date only
+                    x_tick_labels.append(base_labels[i].split(' ')[0])  # Extract date part
+                else:
+                    # For other intervals, show start time of interval
+                    x_tick_labels.append(base_labels[i])
+            
+            # Ensure we don't exceed the resampled length
+            x_tick_labels = x_tick_labels[:resampled_length]
+        
         metadata = GraphCurveMeta(
             title=cls.name if exst_meta.title == "default" else exst_meta.title,
             unit=cls.unit if exst_meta.unit == "default" else exst_meta.unit,
@@ -143,11 +170,16 @@ class AbstractResultCurveGraph(AbstractResult):
             **exst_meta.model_dump(exclude={"title", "unit", "xTickLabels", "properties"}),
         )
 
-        return graph_data, metadata
+        return metadata
 
     @classmethod
-    def make_graph(cls, context: ContextProvider) -> GraphCurveResponse:
-        """Make a curve graph for a specific filtered municipality based on the graph focus in the context"""
+    def make_graph(cls, context: ContextProvider, resample_hours: int = 24) -> GraphCurveResponse:
+        """Make a curve graph for a specific filtered municipality based on the graph focus in the context
+        
+        Args:
+            context: ContextProvider with scenario data
+            resample_hours: Number of hours to aggregate data into (default: 1 for hourly data)
+        """
         region_to_id = region_to_id_map(request=context.request)
 
         if context.request.viewSettings.graphFocus is not None:
@@ -157,7 +189,8 @@ class AbstractResultCurveGraph(AbstractResult):
             all_graphs: list[GraphCurveElement] = cls.graph(context)
             filtered_graph = [ge.filter_on_index(graph_index) for ge in all_graphs]
 
-            graph_data, graph_meta_data = cls.transform_and_create_metadata(filtered_graph)
+            graph_data = cls.transform_data_for_frontend(filtered_graph, resample_hours)
+            graph_meta_data = cls._make_metadata(filtered_graph, resample_hours)
 
             response = GraphCurveResponse(
                 graphData=graph_data,
@@ -175,8 +208,13 @@ class AbstractResultCurveGraph(AbstractResult):
             )
 
     @classmethod
-    def make_graph_toplevel(cls, context: ContextProvider) -> GraphCurveResponse:
-        """Make a top level (province) curve graph, summing all graph curve elements (municipalities) in the context"""
+    def make_graph_toplevel(cls, context: ContextProvider, resample_hours: int = 24) -> GraphCurveResponse:
+        """Make a top level (province) curve graph, summing all graph curve elements (municipalities) in the context
+        
+        Args:
+            context: ContextProvider with scenario data
+            resample_hours: Number of hours to aggregate data into (default: 1 for hourly data)
+        """
         # TODO: This is a temporary solution, we need to make a proper aggregate graph but that is currently out of scope
         all_graphs: list[GraphCurveElement] = cls.graph(context)
 
@@ -190,7 +228,8 @@ class AbstractResultCurveGraph(AbstractResult):
 
         logging.info(f"Summed graph curve elements for toplevel graph: {summed_graphs}")
 
-        graph_data, graph_meta_data = cls.transform_and_create_metadata(summed_graphs)
+        graph_data = cls.transform_data_for_frontend(summed_graphs, resample_hours)
+        graph_meta_data = cls._make_metadata(summed_graphs, resample_hours)
         
         response = GraphCurveResponse(
             graphData=graph_data,
